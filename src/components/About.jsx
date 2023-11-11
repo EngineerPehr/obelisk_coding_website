0af15e9413984c38fd7ffd77057b287851997c55
EngineerPehr <dragontoothforge@gmail.com>
2023-11-11 13:07:05 -0700

    Updated hover effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,41 +10,41 @@ function About () {
                 </p>
                 <h4 className='text-3xl font-semibold mb-3 text-flame'>Skills:</h4>
                 <div className='grid grid-cols-3 sm:flex sm:flex-row sm:justify-around'>
-                    <div className='breathe animate-delay-[0.5s] text-center mb-2'>
+                    <div className='breathe animate-delay-[0.5s] text-center mb-2 group'>
                         <FaJsSquare color='#F7DF1E' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>JavaScript</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>JavaScript</span>
                     </div>
-                    <div className='breathe animate-delay-[1s] text-center mb-2'>
+                    <div className='breathe animate-delay-[1s] text-center mb-2 group'>
                         <FaNodeJs color='#6CC24A' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>NodeJS</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>NodeJS</span>
                     </div>
-                    <div className='breathe animate-delay-[1.5s] text-center mb-2'>
+                    <div className='breathe animate-delay-[1.5s] text-center mb-2 group'>
                         <FaReact color='#5ED4f4' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>ReactJS</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>ReactJS</span>
                     </div>
-                    <div className='breathe animate-delay-[2s] text-center mb-2'>
+                    <div className='breathe animate-delay-[2s] text-center mb-2 group'>
                         <FaPython color='#FFDE57' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>Python3</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>Python3</span>
                     </div>
-                    <div className='breathe animate-delay-[2.5s] text-center mb-2'>
+                    <div className='breathe animate-delay-[2.5s] text-center mb-2  group'>
                         <FaHtml5 color='#F06529' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>HTML 5</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>HTML 5</span>
                     </div>
-                    <div className='breathe animate-delay-[3s] text-center mb-2'>
+                    <div className='breathe animate-delay-[3s] text-center mb-2 group'>
                         <FaCss3 color='#264DE4' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>CSS 3</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>CSS 3</span>
                     </div>
-                    <div className='breathe animate-delay-[3.5s] text-center mb-2'>
+                    <div className='breathe animate-delay-[3.5s] text-center mb-2 group'>
                         <SiTailwindcss color='#38BDF8' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>Tailwind CSS</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>Tailwind CSS</span>
                     </div>
-                    <div className='breathe animate-delay-[4s] text-center mb-2'>
+                    <div className='breathe animate-delay-[4s] text-center mb-2 group'>
                         <FaGitAlt color='#DD0031' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>Git</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>Git</span>
                     </div>
-                    <div className='breathe animate-delay-[4.5s] text-center mb-2'>
+                    <div className='breathe animate-delay-[4.5s] text-center mb-2 group'>
                         <SiPostgresql color='#0064A5' className='icon'/>
-                        <span className='opacity-0 md:opacity-100 hover:opacity-100'>PostgreSQL</span>
+                        <span className='opacity-0 md:opacity-100 group-hover:opacity-100'>PostgreSQL</span>
                     </div>
                 </div>
             </div>
